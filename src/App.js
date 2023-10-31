44082cea45dd9e744a5dc30cf1e6114292217236
lohith1507 <lohith15@gmail.com>
2023-10-31 07:38:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+
+import './App.css';
+import Hello from './components/Hello';
+import data from './data';
+
+// console.log(data);
+
+function App() {
+   return <Hello/>
+  //(
+  //   <main className='container'>
+  //     <ul className='comments-container'>
+  //       {
+  //         data.map((eachComment) => {
+  //           const { id, email, name, body} = eachComment;
+  //           return <li className='comment'>
+  //             <div className='comment-header'>
+  //                 <h3 className='email'>{email}</h3>
+  //                 <h3 className='name'>{name}</h3>
+  //             </div>
+  //             <div className='message'>
+  //               {body}
+  //             </div>
+  //           </li>
+  //         })
+  //       }
+  //       {/* <li className='comment'>
+  //         <div className='comment-header'>
+  //           <h3 className='email'>abc123@gmail.com</h3>
+  //           <h3 className='name'>Type your comment here...</h3>
+  //         </div>
+  //         <p className='message'>
+  //           Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+  //           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+  //         </p>
+  //       </li>
+  //       <li className='comment'>
+  //         <div className='comment-header'>
+  //           <h3 className='email'>abc123@gmail.com</h3>
+  //           <h3 className='name'>Type your comment here...</h3>
+  //         </div>
+  //         <p className='message'>
+  //           Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+  //           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+  //         </p>
+  //       </li>
+  //      */}
+        
+  //     </ul>
+  //   </main>
+  // );
+}
+
+export default App;
---
+++ src/components/Hello.js
@@ -0,0 +1,61 @@
+import React from "react";
+import data from "../data";
+
+//jsx
+const Hello = () =>{
+    // return (
+    //     <main className='container'>
+    //   <ul className='comments-container'>
+    //     {
+    //       data.map((eachComment) => {
+    //         const { id, email, name, body} = eachComment;
+    //         return <li className='comment'>
+    //           <div className='comment-header'>
+    //               <h3 className='email'>{email}</h3>
+    //               <h3 className='name'>{name}</h3>
+    //           </div>
+    //           <div className='message'>
+    //             {body}
+    //           </div>
+    //         </li>
+    //       })
+    //     }
+        
+        
+    //   </ul>
+    // </main>
+    // )
+
+    function clickHandle(firstName, e){
+      console.log("Hey Lohith" ,firstName, e )
+    }
+    return(
+      <main className="container">
+        <button className="button" onClick={function(e){
+          return clickHandle("kandikattu" , e)
+        }}> Click Me</button>
+      </main>
+    )
+
+
+
+
+
+}
+
+// const Hello = () =>{
+//     return (
+//     React.createElement('h1', {className:"heading"}, "Hello World!!!")
+//     );
+// }
+
+// const Hello = () => {
+//     return React.createElement(
+//         "div",
+//         {className:"container"},
+//         React.createElement('h1', {className:'heading'},
+//         "Hello World")
+//     )
+// }
+
+export default Hello;
