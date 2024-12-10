4e83163a0ee79855e665a83f99f6a64b084e6fae
lohith1507 <87384198+lohith1507@users.noreply.github.com>
2024-12-10 10:16:44 +0530

    Add files via upload

---
+++ KitchenTimer/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import TimeDisplay from './components/TimeDisplay';
+
+function App() {
+  return (
+    <TimeDisplay />
+  );
+}
+
+export default App;
