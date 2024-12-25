075d24d0f6874f1c4939624e7b0a9008796a26df
Lohith Nani <lohithnani15@gmail.com>
2024-12-25 09:16:15 +0530

    todo-start

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-
-import { useEffect, useState } from 'react';
 import './App.css';
-// import Hello from './components/Hello';
-// import data from './data';
-// import Final from './useState/basics1';
-// import Index from './useState/Advanced';
-// import Index from './useState/Example-3';
-// import Final from './useEffect/example-1'
-// import Final from "./useEffect/with dep";
-import Index  from './useEffect/example-2';
+import Index from './useState/Example-3';
+
 
 
 function App() {
